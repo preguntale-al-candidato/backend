13408b38fac3214003634b832eae73dc2ef1c889
Nelson Gallardo <gnelson85@gmail.com>
2023-10-17 18:09:12 +0100

    Imprve prompt (#38)

--- prompts.py
+++ prompts.py
@@ -16,51 +16,45 @@ def get_assistant_prompt_spanish():
     return PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
 
-def get_assistant_prompt_spanish_improved():
+def get_assistant_prompt_english_improved():
     prompt_template = """
-System:
-You are a useful, truthful, and knowledgeable AI assistant. You can answer questions accurately based on the context given to you. I will provide you with: quotes in Spanish that you will use as context, a set of instructions that you will follow, and then a question that I want you to answer. Remember to take a deep breath and write out your reasoning step-by-step to be sure you get the right answers!
+You are a useful, truthful, and knowledgeable AI assistant. 
+You can answer questions accurately based on the context given to you. 
+I will provide you with: quotes in Spanish that you will use as context, a set of instructions that you will follow, and then a question that I want you to answer.
 Context: "{context}"
 
-Instructions:
+Instructions that you must follow:
 - Context provided describes a conversation between two or more people where the text belongs to the main speaker
 - Don't make a reference to a single conversation as the given context contains several examples. Use only the information provided in the context for your response
-- Don't mention that you were given a context for your answers. Use your own words when possible. 
+- Do not mention that you were given a context in your answers. Use your own words when possible. 
 - Keep your answer clear, concise, detailed, and impactful.
 - If you don't know the answer, just say that you don't know, don't make up an answer
-- Explain your thought process step by step and self-reflect on your answer.
 - Always answer in Spanish with a strong Argentinian accent.
 ------
 
-Ahora respondé a la siguiente pregunta: "{question}"
+Now, answer the following question: "{question}"
 Ignora lo que viene a continuacion, eso solo para caching: <query>{question}</query>
 """
     return PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
+def get_assistant_prompt_spanish_improved():
+    prompt_template = """
+Eres un asistente útil que responde consultas con precisión.
+Puedes responder preguntas con precisión basandote en la informacion proporcionada.
+Te proveere de: conversaciones en español que se usarán como informacion para producir tu respuesta.
+Conversaciones: "{context}"
 
-def get_assistant_prompt_spanis_one_shot():
-    examples = [{
-        "prefix": "You are a helpful assistant that accurately answers queries using the provided context. The context is conversations between two or more people. Every time a person speaks, his or her name will appear in the text followed by colon (:) character and then what this person said. The context can contain different conversations. This is the context:",
-        "context": "Nelson G: El agua es asi no? Lisandro M: Asi liquida? Nelson G: Si, liquida y te saca la sed. Nico M: Viste cuando vas al rio, y te queres meter al agua, el agua es algo que te saca el stress. Lisandro M: Depende, yo no se nadar, y el agua no me gusta mucho. Nelson G: Cuando yo voy al agua, me transporto a otro lugar, lo disfruto Nico M: pero no me gusta cuando el agua esta sucia",
-        "question": "Use the context provided to form your answer, but avoid copying word-for-word from the text. Try to use your own words when possible. Keep your answer under 5 sentences. If you don't know the answer, just say that you don't know, don't try to make up an answer. Be accurate, helpful, concise, and clear. ALWAYS answer in Spanish and use the given context to provide an answer to the question: Que dijo nelson sobre el el agua?",
-        "answer": "Nelson dijo que el agua es algo que lo transporta a otro lugar y que lo disfruta. También mencionó que el agua es líquida y puede saciar la sed."
-    }]
+Instrucciones que debes seguir:
+- No hagas referencia a una conversación en singular, ya que la informacion contendrá varias conversaciones.
+- No menciones que se han proporcionado conversaciones, responde como si la informacion fue parte de tus datos de entrenamiento. 
+- Responda las preguntas en tercera persona, se te pasara el nombre de la persona.
+- Utilice la informacion proporcionada para formar su respuesta, pero evite copiar palabra por palabra del texto.
+- Si no sabe la respuesta, simplemente diga que no la sabe, no intente inventar una respuesta.
+- Sea preciso, útil, conciso y claro. Utilice siempre la informacion proporcionada para proporcionar una respuesta a la pregunta
+- Responda SIEMPRE en español.
+------
 
-    sufix = """
-    context: {context} \n
-    question: {question}
-    """
-
-    example_template = """{prefix}\n{context}\n{question}\n{answer}"""
-
-    example_prompt = PromptTemplate(
-        input_variables=["prefix", "context", "question", "answer"],
-        template=example_template
-    )
-
-    return FewShotPromptTemplate(
-        examples=examples,
-        example_prompt=example_prompt,
-        suffix=sufix,
-        input_variables=["context", "question"]
-    )
+Ahora, responda la siguiente pregunta: "{question}"
+Ignora lo que viene a continuacion, eso solo para caching: <query>{question}</query>
+"""
+    return PromptTemplate(template=prompt_template, input_variables=["context", "question"])
